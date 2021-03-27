ccafcf2c0836c22408394fdd291c86e106e029f7
Shiwei Ge <ajie4095@gmail.com>
2021-03-27 00:15:08 -0400

    Update app.js

--- src/website/app.js
+++ src/website/app.js
@@ -1,51 +1,68 @@
-const express =require('express');
+const express = require("express");
+
 const app = express();
 
 const PORT = process.env.PORT || 4001;
 
-const fs = require('fs');
-const bodyParser = require('body-parser')
+const fs = require("fs");
 
+const bodyParser = require("body-parser");
 
-let text = fs.readFileSync('./index.txt').toString('utf-8');
-let newText = text.replace(/\s/g, '');
-let dataArray = newText.split(',');
+const { error } = require("console");
+
+let text = fs.readFileSync("./index.txt").toString("utf-8");
+let newText = text.replace(/\s/g, "");
+let dataArray = newText.split(",");
 let data = {};
-data.sensorData = {humidity: dataArray[0], temperature: dataArray[1], lightIntensity: dataArray[2], windSpeed: dataArray[3]};
+data.sensorData = {
+    humidity: dataArray[0],
+    temperature: dataArray[1],
+    lightIntensity: dataArray[2],
+    windSpeed: dataArray[3],
+};
 data.userSetting = {};
 
-
-setInterval(()=> {
-    let updatedText = fs.readFileSync('./index.txt').toString('utf-8');
-    let updatedNewText = text.replace(/\s/g, '');
-    let updatedDataArray = newText.split(',');
+// read the index file per secdon in order for the in-time updating the sensor data
+setInterval(() => {
+    let updatedText = fs.readFileSync("./index.txt").toString("utf-8");
+    let updatedNewText = text.replace(/\s/g, "");
+    let updatedDataArray = newText.split(",");
     text = updatedText;
     newText = updatedNewText;
     dataArray = updatedDataArray;
-    data.sensorData = {humidity: dataArray[0], temperature: dataArray[1], lightIntensity: dataArray[2], windSpeed: dataArray[3]};
+    data.sensorData = {
+        humidity: dataArray[0],
+        temperature: dataArray[1],
+        lightIntensity: dataArray[2],
+        windSpeed: dataArray[3],
+    };
 }, 1000);
 
 app.use(bodyParser.json());
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get('/sensorData', (req, res, next) => {
-    if(dataArray.length > 0) {
+app.get("/sensorData", (req, res, next) => {
+    if (dataArray.length > 0) {
         res.send(data);
     } else {
-        res.status(404).send('Not found');
+        res.status(404).send("Not found");
     }
-})
+});
 
-app.put('/sensorData', (req, res, next) => {
-    data.userSetting = req.body.userSetting
-    // data.userSetting = {temperature: req.query.temperature, humidity: req.query.humidity, windSpeed: req.query.windSpeed, lightIntensity: req.query.lightIntensity}
-    res.send(data);
-})
- 
-
-
+app.put("/sensorData", (req, res, next) => {
+    try {
+        let existence = req.body.userSetting;
+        if (existence !== "") {
+            data.userSetting = req.body.userSetting;
+            return res.send(data);
+        }
+        throw new Error("new error found!");
+    } catch (Error) {
+        console.log(Error);
+    }
+});
 
 app.listen(PORT, () => {
-    console.log('server connected successfully!');
-})
+    console.log("server connected successfully!");
+});
